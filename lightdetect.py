5ff9eca31cdece356f1b4522ba142f13b6648e3c
jinalee-mech <90688988+jinalee-mech@users.noreply.github.com>
2022-11-20 15:04:35 +0900

    Update lightdetect.py

--- lightdetect.py
+++ lightdetect.py
@@ -51,9 +51,6 @@ time.sleep(0.1)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
 
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
-
     # 영상 블러, 에지 부각
     # hsv_img = np.zeros((H, W), image.dtype)
     # gray_img = np.zeros((H, W), image.dtype)
@@ -66,62 +63,73 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     filtered_img = cv2.bilateralFilter(gray_img, -1, sigmaColor, sigmaSpace)
 
     # 원 검출
-    circles = cv2.HoughCircles(filtered_img, cv2.HOUGH_GRADIENT, 1, 80,
-                               param1=70, param2=28, minRadius=15, maxRadius=100)
-    circles = circles[0]
+    circles = cv2.HoughCircles(filtered_img, cv2.HOUGH_GRADIENT, 5, 100,
+                               param1=50, param2=300, minRadius= 50, maxRadius=60)
 
-    for i in range(len(circles)):
-        print(circles[i][0]) # 각 원 순서대로 중심점 출력
+    if (circles is not None):
 
-    # 검출된 원 내부 색상정보
-    for i in range(len(circles)):
-        center = [round(circles[i][0]), round(circles[i][1])]
-        radius = round(circles[i][2])
-        print("center: ", center)
+        circles = circles[0]
 
-        x = center[0]
-        y = center[1]
-        # indexoutofrange 오류 해결
-        if (y == 480 or x == 640):
-            y = 479
-            x = 639
-        print(f"x: {x}, y: {y}")
-        B = image[y][x][0]
-        G = image[y][x][1]
-        R = image[y][x][2]
-        print(f"R: {R}, G: {G}, B: {B}")
+        for i in range(len(circles)):
+            print(circles[i][0]) # 각 원 순서대로 중심점 출력
 
-        if (R-B > 20 and R-G > 20 and R > 100):
-            print("Red Light!! Stop!")
-        if (G-R > 20 and G-B > 20 and G > 100):
-            print("Detected Green Light")
-            CenterX = center[0]
-            CenterY = center[1]
-            Cradius = radius
-            Width = 2*Cradius
-            Height = 2*Cradius
-            TotalPixel = math.floor(PI*Cradius*Cradius)
-            green_cnt_r = 0
-            green_cnt_l = 0
+        # 검출된 원 내부 색상정보
+        for i in range(len(circles)):
+            center = [round(circles[i][0]), round(circles[i][1])]
+            radius = round(circles[i][2])
+            print("center: ", center)
 
-            for k in range(CenterX-Width, CenterX+Width+1):
-                for j in range(CenterY-Height, CenterY+Height+1):
-                    blue_data = image[j, k, 0]
-                    green_data = image[j, k, 1]
-                    red_data = image[j, k, 2]
+            x = center[0]
+            y = center[1]
+            # indexoutofrange 오류 해결
+            if (y == 480 or x == 640):
+                y = 479
+                x = 639
+            print(f"x: {x}, y: {y}")
+            B = image[y][x][0]
+            G = image[y][x][1]
+            R = image[y][x][2]
+            print(f"R: {R}, G: {G}, B: {B}")
 
-                    if (green_data-blue_data > 20 and green_data-red_data > 20 and green_data > 100):
-                        if (k <= CenterX): green_cnt_l += 1
-                        else: green_cnt_r += 1
+            if (R-B > 20 and R-G > 20 and R > 100):
+                print("Red Light!! Stop!")
+                cv2.line(image, (x, y), (x, y), (255, 0, 0), 5)  # 빨간원으로 인식했을때 중점찍기
+            if (G-R > 20 and G-B > 20 and G > 100):
+                print("Detected Green Light")
+                CenterX = center[0]
+                CenterY = center[1]
+                if (CenterY == 480 or CenterX == 640):
+                    CenterY = 479
+                    CenterX = 639
+                Cradius = radius
+                Width = 2*Cradius
+                Height = 2*Cradius
+                TotalPixel = math.floor(PI*Cradius*Cradius)
+                green_cnt_r = 0
+                green_cnt_l = 0
+                cv2.line(image, (x, y), (x, y), (0, 0, 255), 5)  # 초록원으로 인식했을때 중점찍기
 
-            print(f'green_cnt: {green_cnt_r + green_cnt_l} Total Pixel: {TotalPixel}')
-            if (TotalPixel*0.6 < green_cnt_r + green_cnt_l):
-                if (green_cnt_r > green_cnt_l): print("Go Right ->")
-                else: print("Go Left <-")
+                for k in range(CenterX-Width, CenterX+Width+1):
+                    for j in range(CenterY-Height, CenterY+Height+1):
+                        blue_data = image[j, k, 0]
+                        green_data = image[j, k, 1]
+                        red_data = image[j, k, 2]
 
-        else: print("stay stop!")
+                        if (green_data-blue_data > 20 and green_data-red_data > 20 and green_data > 100):
+                            if (k <= CenterX): green_cnt_l += 1
+                            else: green_cnt_r += 1
 
-    rawCapture.truncate(0) # clear stream
+                print(f'green_cnt: {green_cnt_r + green_cnt_l} Total Pixel: {TotalPixel}')
+                if (TotalPixel*0.6 < green_cnt_r + green_cnt_l):
+                    if (green_cnt_r > green_cnt_l): print("Go Right ->")
+                    else: print("Go Left <-")
+
+            else: print("stay stop!")
+            cv2.circle(image, (x, y), radius, (255, 0, 0), 5)  # 원그리기
+
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+    rawCapture.truncate(0)  # clear stream
 
     if key == ord("q"):
         break
